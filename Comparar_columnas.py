8e44287103cf95c0c562293a8b5a1a5035ce97f2
Santiago <santiagogarcia2711@gmail.com>
2023-10-07 16:35:41 -0500

    trabajo final

--- Comparar_columnas.py
+++ Comparar_columnas.py
@@ -1,31 +1,46 @@
 import pandas as pd
-Cadenaacomparar=["maria","juan","juana"]
+import hora
+
 
 doc=pd.read_excel("hoja.xlsx")
 
 df=pd.DataFrame(doc)
-print(df)
+
+horaminima=int(input("¿Cuales son los minutos minimos de asistencia? "))
+
 m=-1
 presente=[""]*len(df)
 Col1=[""]*len(df)
 Col2=[""]*len(df)
+Col3=[""]*len(df)
 
 for i in range(len(df)):
    m=m+1
    Col1[m]=df.iloc[i]['Correos misestu']
    Col2[m]=df.iloc[i]['Correos asistentes']
+   Col3[m]=df.iloc[i]['Duracion']
+
 
 m=-1
+minutosfinal=[""]*len(Col3)
+hora.tiempoo(Col3,minutosfinal)
+
+
+
 for j in Col1:
    if j=="":
       break
    m=m+1
    presente[m]=0
+   
 
-   for k in Col2:
-      if str(j).lower()==str(k).lower():
-         presente[m]=1
+   for k in range(len(df)):
+      if str(j).lower()==str(Col2[k]).lower():
+         if minutosfinal[k]>horaminima:
+            presente[m]=1
+         elif minutosfinal[k]<=horaminima:
+            presente[m]=0
 
       
-hoja_final=pd.DataFrame({"Correos misestu":Col1,"Presente":presente,"Correos asistentes":Col2})
+hoja_final=pd.DataFrame({"Correos misestu":Col1,"Presente":presente,"Correos asistentes":Col2,"Tiempo de asistencia":Col3,"Tiempo de asistencia minutos":minutosfinal})
 hoja_final.to_excel('Resultado.xlsx', sheet_name='sheet1', index=False)
--- hora.py
+++ hora.py
@@ -1,18 +1,36 @@
-horas=["1 h 24 min","1 h","3 min"]
+def tiempoo(matriz,minutosfinal):
+    conteo=-1
+    for i in matriz:
+        i=str(i)
+        conteo=conteo+1
+        min=0
+        hor=0
+        seg=0
 
-for i in horas:
-    min=0
-    hor=0
+        for k in i:
+            if (k=="m"):
+                min=1
+        for k in i:
+            if (k=="h"):
+                hor=1
+        for k in i:
+            if (k=="s"):
+                seg=1
 
-    for k in i:
-        if (k=="m"):
-            print("tiene minutos")
-            min=1
-        if (k=="h"):
-            print("tiene horas")
-            hor=1
-    if min==1 & hor==0:
-            minutototal=i[0]+i[1]
-            print(minutototal)
+        if (min==1 and hor==0):
+            minutosfinal[conteo]=int(i[0]+i[1])
+            
+        elif (min==0 and hor==1):
+            horasfinal=int(i[0])
+            minutosfinal[conteo]=horasfinal*60
+
+        elif (min==1 and hor==1):
+            horasfinal=int(i[0])
+            minutos=int(i[4]+i[5])
+            minutosfinal[conteo]=horasfinal*60+minutos
+
+        elif (seg==1):
+            minutosfinal[conteo]=0
+        
     
 
